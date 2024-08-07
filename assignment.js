4270b3d87dc3e2cf3c253ed9af8a3fb54d26ac8e
MarkTBSS <marktbss@gmail.com>
2024-08-07 08:10:22 +0700

    [ADD] First Commit

---
+++ assignment.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const neighbours = ['Norway', 'Sweden', 'Russia'];
+
+neighbours.push('Utopia');
+console.log(neighbours);
+
+neighbours.pop();
+console.log(neighbours);
+
+if (!neighbours.includes('Germany')) {
+  console.log('Probably not a central European country :D');
+}
+
+neighbours[neighbours.indexOf('Sweden')] = 'Republic of Sweden';
+console.log(neighbours);
